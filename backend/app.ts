6012671a3eed8ba6941ffc51039bbcb46a481fff
Shridhar <shridharkamat10@gmail.com>
2024-06-28 20:27:41 +0530

    feat: added vercel deploy

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,21 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import channelRouter from "./routes/channel.routes.js"
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const PORT = 8000
+
+mongoose.connect(process.env.DB!).then((con)=>{
+    console.log("CONNECTED TO DB SUCCESSFULLY!")
+})
 
 const app:Express = express()
 app.use(cors({
@@ -19,5 +34,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/user", userRouter)
 app.use("/video", videoRouter)
 app.use("/channel", channelRouter)
+
+app.listen(PORT,()=>{
+  console.log(`SERVER STARTED AT PORT:${PORT}`)
+})
+
 
 export default app
